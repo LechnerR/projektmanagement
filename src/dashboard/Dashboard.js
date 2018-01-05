eaffeda94462bb9198b6d9cf6f3e2c487be43d1b
Florian Gärner <florian.gaerner@gmail.com>
2018-01-05 10:53:14 +0100

    merged

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import {
   Link,
   Switch
 } from 'react-router-dom';
-import { Grid, Row, Col } from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 import 'font-awesome/css/font-awesome.min.css';
 import './Dashboard.css';
 import Detailview from '../project/Detailview.js';
@@ -13,7 +13,9 @@ import NewTask from '../forms/NewTask.js';
 import NewEmployee from '../forms/NewEmployee.js';
 import TaskDetails from '../project/TaskDetails.js';
 
-const Projects = () => {
+var axios = require('axios')
+
+/*const Projects = () => {
   return (
     <Switch>
       <Route path='/dashboard' component={AllProjects} />
@@ -24,71 +26,125 @@ const Projects = () => {
       <Route path='/taskDetails/:id' component={TaskDetails} />
     </Switch>
   )
-}
+}*/
+
+const url = 'https://b0qco5h7cj.execute-api.eu-central-1.amazonaws.com/pm/'
+
 
 class Dashboard extends Component {
 
-  render () {
-    return (
-      <div>
-        <Projects />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {projects: []};
+    }
+
+
+    componentWillMount() {
+        var self = this;
+        axios.get(url + '/Project')
+            .then(function (response) {
+                self.setState({projects: response.data.Items})
+                console.log('componentWillMount - self.state', self.state);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h2>Projektübersicht</h2>
+                <Grid>
+                    <Row className="show-grid">
+                        {
+                            this.state.projects.map(p => (
+                                <Col xs={6} md={4} key={p.ID} className="Project">
+                                    <div className="jumbotron">
+                                        <h3>{p.Title}</h3>
+                                        <p>{p.Description}</p>
+                                        <p><Link className="Details" to={`/projects/${p.ID}`}>Details</Link></p>
+                                    </div>
+                                </Col>
+                            ))
+                        }
+                    </Row>
+                </Grid>
+                <Link to="/newProject" className="Button NewProject"><i id="NewProject"
+                                                                        className="fa fa-plus-circle"></i>neues
+                    Projekt</Link>
+            </div>
+        );
+    }
 }
 
-const ProjectAPI = {
-  projects: [
-    { id: 1, start_date: "1.1.2001", end_date: "1.1.2001", description:"Beschreibung A", title:"Projekt A", notice:"Notiz" },
-    { id: 2, start_date: "1.1.2001", end_date: "1.1.2001", description:"Beschreibung B", title:"Projekt B", notice:"Notiz" },
-    { id: 3, start_date: "1.1.2001", end_date: "1.1.2001", description:"Beschreibung C", title:"Projekt C", notice:"Notiz" },
-    { id: 4, start_date: "1.1.2001", end_date: "1.1.2001", description:"Beschreibung D", title:"Projekt D", notice:"Notiz" },
-    { id: 5, start_date: "1.1.2001", end_date: "1.1.2001", description:"Beschreibung E", title:"Projekt E", notice:"Notiz" }
-    // { id: 6, start_date: "1.1.2001", end_date: "1.1.2001", description:"Projekt F", title:"Projekt F", notice:"Notiz" }
-  ],
-  all: function() { return this.projects},
-  get: function(id) {
-    const isProject = p => p.id === id
-    return this.projects.find(isProject)
-  }
-}
-
-function AllProjects(props) {
-  return (
-    <div>
-      <h2>Projektübersicht</h2>
-      <Grid>
-        <Row className="show-grid">
-          {
-            ProjectAPI.all().map(p => (
-              <Col xs={6} md={4} key={p.id} className="Project">
-                <div className="jumbotron">
-                  <h3>{p.title}</h3>
-                  <p>{p.description}</p>
-                  <p><Link className="Details" to={`/projects/${p.id}`}>Details</Link></p>
-                  // TODO button needs functionality
-                  <button className="Trash" title="Projekt löschen"><i className="fa fa-trash-o"></i></button>
-                </div>
-              </Col>
-            ))
+/*const ProjectAPI = {
+    /!*projects: [
+      { id: 1, start_date: "1.1.2001", end_date: "1.1.2001", description:"Projekt A", title:"Projekt A", notice:"Notiz" },
+      { id: 2, start_date: "1.1.2001", end_date: "1.1.2001", description:"Projekt B", title:"Projekt B", notice:"Notiz" },
+      { id: 3, start_date: "1.1.2001", end_date: "1.1.2001", description:"Projekt C", title:"Projekt C", notice:"Notiz" },
+      { id: 4, start_date: "1.1.2001", end_date: "1.1.2001", description:"Projekt D", title:"Projekt D", notice:"Notiz" },
+      { id: 5, start_date: "1.1.2001", end_date: "1.1.2001", description:"Projekt E", title:"Projekt E", notice:"Notiz" },
+      { id: 6, start_date: "1.1.2001", end_date: "1.1.2001", description:"Projekt F", title:"Projekt F", notice:"Notiz" }
+    ],*!/
+    loadAll: function () {
+        axios.get(url + '/Project'/!*, {
+          params: {
+              ID: 12345
           }
-        </Row>
-      </Grid>
-      <Link to="/newProject" className="Button NewProject"><i id="NewProject" className="fa fa-plus-circle"></i>neues Projekt</Link>
-    </div>
-  );
+      }*!/)
+            .then(function (response) {
+                this.state.projects = response.data.Items;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },
+    get: function (id) {
+        const isProject = p => p.ID === id
+        return this.state.projects.find(isProject)
+    }
+}*/
+
+
+/*function AllProjects(props) {
+    console.log('AllProjects - props', props);
+    console.log('AllProjects - this.state', this.state);
+    return (
+        <div>
+            <h2>Projektübersicht</h2>
+            <Grid>
+                <Row className="show-grid">
+                    {
+                        this.state.projects.map(p => (
+                            <Col xs={6} md={4} key={p.ID} className="Project">
+                                <div className="jumbotron">
+                                    <h3>{p.Title}</h3>
+                                    <p>{p.Description}</p>
+                                    <p><Link className="Details" to={`/projects/${p.ID}`}>Details</Link></p>
+                                </div>
+                            </Col>
+                        ))
+                    }
+                </Row>
+            </Grid>
+            <Link to="/newProject" className="Button NewProject"><i id="NewProject" className="fa fa-plus-circle"></i>neues
+                Projekt</Link>
+        </div>
+    );
 }
 
 function Project(props) {
-  const project = ProjectAPI.get(
-    parseInt(props.match.params.id,10)
-  )
-  if (!project) {
-    return <div>Sorry, but the project was not found</div>
-  }
-  return (
-    <Detailview project={project} />
-  )
-}
+    const project = ProjectAPI.get(
+        parseInt(props.match.params.ID, 10)
+    )
+    if (!project) {
+        return <div>Sorry, but the project was not found</div>
+    }
+    return (
+        <Detailview project={project}/>
+    )
+}*/
 
 export default Dashboard;
